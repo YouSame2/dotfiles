602ede6854023c11d1e106c5fd29ddaa02c14666
YouSame2 <businessyousame@gmail.com>
2025-02-22 19:45:44 -0600

    add more keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,22 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move selected line down 1
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move selected line up 1
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- half page down center
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- half page up center
-vim.keymap.set("n", "n", "nzzzv") -- center on search next
-vim.keymap.set("n", "N", "Nzzzv") -- center on search prev
-vim.keymap.set("x", "<leader>p", [["_dP]]) -- paste over visual selection but keep paste register
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")      -- half page down center
+vim.keymap.set("n", "<C-u>", "<C-u>zz")      -- half page up center
+vim.keymap.set("n", "n", "nzzzv")            -- center on search next
+vim.keymap.set("n", "N", "Nzzzv")            -- center on search prev
+
+vim.keymap.set("x", "<leader>p", [["_dP]])   -- paste over visual selection but keep paste register
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+
+
+
+-- TODO:
+
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- sub current word under cursor
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -8,32 +8,40 @@ return {
     -- "hrsh7th/cmp-nvim-lsp", -- using blink instead
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    'saghen/blink.cmp',
+    "saghen/blink.cmp",
   },
 
   config = function()
     -- == LSP keymaps only after LSP Attach == --
     local on_attach_keys = function(_, bufnr)
+      -- TODO: add cnext cprev
       local keymap = vim.keymap
       local function opts(desc)
         return { buffer = bufnr, desc = "LSP: " .. desc }
       end
       -- keymaps
-      keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
       keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
-      keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Go to implementation"))
-      keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help, opts("Show signature help"))
+      keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts("Go to implementation (Tele)"))
+      keymap.set("n", "gI", vim.lsp.buf.implementation, opts("Go to implementation"))
+      keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts("Show references (Tele)"))
+      keymap.set("n", "gR", vim.lsp.buf.references, opts("Show references"))
       keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
       keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
       keymap.set("n", "<leader>wl", function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
       end, opts("List workspace folders"))
-      keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts("Go to type definition"))
+      keymap.set(
+        "n",
+        "<leader>ct",
+        "<cmd>Telescope lsp_type_definitions<CR>",
+        opts("Go to type Definition (Tele)")
+      )
+      keymap.set("n", "<leader>cT", vim.lsp.buf.type_definition, opts("Go to type definition"))
       keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
-      keymap.set("n", "gr", vim.lsp.buf.references, opts("Show references"))
-      keymap.set("n", "<space>f", function()
-        vim.lsp.buf.format { async = true }
-      end, opts("Format document"))
+      keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts("Smart rename"))
+      keymap.set("n", "<leader>cd", "<cmd>Telescope diagnostics bufnr=0<CR>", opts("show diagnostics (Tele)"))
+      keymap.set("n", "<leader>cs", vim.lsp.buf.signature_help, opts("Show signature help"))
     end
 
     -- Change the Diagnostic symbols in the sign column (gutter)
@@ -49,7 +57,7 @@ return {
 
     -- == Configure LSP servers == --
     mason_lspconfig.setup_handlers({
-      function(server_name)  -- Default handler (all servers)
+      function(server_name) -- Default handler (all servers)
         lspconfig[server_name].setup({
           on_attach = on_attach_keys,
           capabilities = capabilities,
@@ -75,60 +83,5 @@ return {
       --   })
       -- end,
     })
-
   end,
 }
-
-
-
--- TODO: take whats good credit: https://github.com/josean-dev
--- 
--- local keymap = vim.keymap -- for conciseness
--- vim.api.nvim_create_autocmd("LspAttach", {
---   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
---   callback = function(ev)
---     -- Buffer local mappings.
---     -- See `:help vim.lsp.*` for documentation on any of the below functions
---     local opts = { buffer = ev.buf, silent = true }
---
---     -- set keybinds
---     opts.desc = "Show LSP references"
---     keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
---
---     opts.desc = "Go to declaration"
---     keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
---
---     opts.desc = "Show LSP definitions"
---     keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
---
---     opts.desc = "Show LSP implementations"
---     keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
---
---     opts.desc = "Show LSP type definitions"
---     keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
---
---     opts.desc = "See available code actions"
---     keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
---
---     opts.desc = "Smart rename"
---     keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
---
---     opts.desc = "Show buffer diagnostics"
---     keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
---
---     opts.desc = "Show line diagnostics"
---     keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
---
---     opts.desc = "Go to previous diagnostic"
---     keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
---
---     opts.desc = "Go to next diagnostic"
---     keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
---
---     opts.desc = "Show documentation for what is under cursor"
---     keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
---
---     opts.desc = "Restart LSP"
---     keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
---   end,
--- })

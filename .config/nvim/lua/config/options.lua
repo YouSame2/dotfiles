eb58295a2c2ccf2df34bcf62b5a565ea86729d80
YouSame2 <businessyousame@gmail.com>
2025-05-27 17:57:42 -0700

    YEET dotfiles

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -221,7 +221,7 @@ local function set_diff_highlights()
 		vim.api.nvim_set_hl(0, "DiffAdd", { fg = "none", bg = "#2e4b2e", bold = true })
 		vim.api.nvim_set_hl(0, "DiffDelete", { fg = "none", bg = "#4c1e15", bold = true })
 		vim.api.nvim_set_hl(0, "DiffChange", { fg = "none", bg = "#45565c", bold = true })
-		vim.api.nvim_set_hl(0, "DiffText", { fg = "none", bg = "#996d74", bold = true })
+		vim.api.nvim_set_hl(0, "DiffText", { fg = "none", bg = "#5a3f43", bold = true })
 	else
 		vim.api.nvim_set_hl(0, "DiffAdd", { fg = "none", bg = "palegreen", bold = true })
 		vim.api.nvim_set_hl(0, "DiffDelete", { fg = "none", bg = "tomato", bold = true })
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -27,13 +27,15 @@ return {
 			},
 			highlight = {
 				enable = true,
-				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
+				-- NOTE: enable below function if running into performance issues on large files:
+
+				-- disable = function(lang, buf)
+				-- 	local max_filesize = 100 * 1024 -- 100 KB
+				-- 	local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				-- 	if ok and stats and stats.size > max_filesize then
+				-- 		return true
+				-- 	end
+				-- end,
 			},
 			incremental_selection = {
 				enable = true,
